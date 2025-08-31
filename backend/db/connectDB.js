b2d091c11e43bcbfa3a638b9d46866f980696bdb
BHUVAN <bhuvanbn01@gmail.com>
2025-08-31 18:53:36 +0530

    corrected mongoose warnings

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -4,10 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 export const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGODB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
